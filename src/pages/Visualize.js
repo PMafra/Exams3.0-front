8bee12f5f33e251ae268bcce1c3609cbfbf39f4a
PMafra <pmafrarm@gmail.com>
2021-12-16 16:57:42 -0300

    Add logic to filter by professor or discipline; add reset button; still not getting all data from server

--- src/pages/Visualize.js
+++ src/pages/Visualize.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-console */
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
+import { IoCloseCircleOutline } from 'react-icons/io5';
 import { getSchools } from '../services/api';
 
 export default function Visualize() {
@@ -12,7 +13,10 @@ export default function Visualize() {
   const [typesList, setTypesList] = useState([]);
   const [semestersList, setSemestersList] = useState([]);
 
-  const [chosenSchool, setChosenSchool] = useState(false);
+  const [chosenSchool, setChosenSchool] = useState('');
+
+  const [isProfessorFilter, setIsProfessorFilter] = useState(true);
+  const [isDisciplineFilter, setIsDisciplineFilter] = useState(false);
 
   const requestSchools = () => {
     getSchools()
@@ -32,6 +36,11 @@ export default function Visualize() {
     e.PreventDefault();
   };
 
+  const defineFilter = () => {
+    setIsProfessorFilter(!isProfessorFilter);
+    setIsDisciplineFilter(!isDisciplineFilter);
+  };
+
   return (
     <StyledPageContainer>
       <StyledMainContent>
@@ -39,13 +48,42 @@ export default function Visualize() {
           <StyledForm onSubmit={(e) => requestFilteredExams(e)}>
             <label htmlFor="filters" className="inputs-container">
               <h2>Select the filters:</h2>
-              <input list="schools" name="schools" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} onSelect={(e) => setChosenSchool(e.target.value)} />
-              {chosenSchool ? (
+              <div className="input-box">
+                <input list="schools" name="schools" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} onSelect={(e) => setChosenSchool(e.target.value)} />
+                <IoCloseCircleOutline className="x-icon" onClick={() => window.location.reload()} />
+              </div>
+              {chosenSchool !== '' ? (
                 <>
-                  <input list="professors" name="professors" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
-                  <input list="types" name="types" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
-                  <input list="disciplines" name="disciplines" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
-                  <input list="semesters" name="semesters" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
+                  <StyledPublicButtonsContainer
+                    isDisciplineFilter={isDisciplineFilter}
+                    isProfessorFilter={isProfessorFilter}
+                  >
+                    <StyledButton
+                      className="professor"
+                      onClick={() => defineFilter()}
+                    >
+                      Filter by professor
+                    </StyledButton>
+                    <StyledButton
+                      className="discipline"
+                      onClick={() => defineFilter()}
+                    >
+                      Filter by discipline
+                    </StyledButton>
+                  </StyledPublicButtonsContainer>
+                  {isProfessorFilter ? (
+                    <>
+                      <input list="professors" name="professors" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
+                      <input list="types" name="types" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
+                    </>
+                  ) : ('')}
+                  {isDisciplineFilter ? (
+                    <>
+                      <input list="types" name="types" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
+                      <input list="disciplines" name="disciplines" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
+                      <input list="semesters" name="semesters" id="filters" type="text" onKeyDown={(e) => e.preventDefault()} />
+                    </>
+                  ) : ('')}
                 </>
               ) : ('')}
             </label>
@@ -74,7 +112,9 @@ export default function Visualize() {
                 <option value={school} />
               ))}
             </datalist>
-            <input type="submit" className="submit" />
+            {false ? (
+              <input type="submit" className="submit" />
+            ) : ('')}
           </StyledForm>
         </StyledFormContainer>
       </StyledMainContent>
@@ -118,7 +158,18 @@ const StyledForm = styled.form`
       font-size: 30px;
       margin-bottom:2vh;
     }
-    input {
+    .input-box {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    justify-content: center;
+    .x-icon {
+      font-size: 45px;
+      color: red;
+    }
+    }
+  }
+  input {
       width: 350px;
       height: 40px;
       border-radius: 20px;
@@ -129,7 +180,6 @@ const StyledForm = styled.form`
         outline: none;
       }
     }
-  }
   .submit {
     width: 200px;
     height:70px;
@@ -143,5 +193,37 @@ const StyledForm = styled.form`
     &:hover {
       opacity: 0.8;
     }
+  }
+`;
+const StyledPublicButtonsContainer = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  gap: 18vw;
+  .login {
+    color: #434871;
+  }
+  .professor {
+    background-color: ${({ isProfessorFilter }) => (isProfessorFilter ? ('#bfbafc') : ('#878787'))};
+  }
+  .discipline {
+    background-color: ${({ isDisciplineFilter }) => (isDisciplineFilter ? ('#bfbafc') : ('#878787'))};
+  }
+`;
+const StyledButton = styled.div`
+  padding: 0 20px;
+  width: 200px;
+  height:70px;
+  border-radius: 60px;
+  font-size: 20px;
+  font-weight: 700;
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  text-align: center;
+  &:hover {
+    opacity: 0.8;
   }
 `;
