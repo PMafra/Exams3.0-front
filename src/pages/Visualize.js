f23b711cee32bc4ba5508448e7c4b02c6795cd02
PMafra <pmafrarm@gmail.com>
2021-12-16 12:07:29 -0300

    Create input filters page first layout

--- src/pages/Visualize.js
+++ src/pages/Visualize.js
@@ -1,10 +1,76 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable no-console */
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { getFiltersInfo } from '../services/api';
 
 export default function Visualize() {
+  const [schoolsList, setSchoolsList] = useState([]);
+  const [professorsList, setProfessorsList] = useState([]);
+  const [disciplinesList, setDisciplinesList] = useState([]);
+  const [typesList, setTypesList] = useState([]);
+  const [semestersList, setSemestersList] = useState([]);
+
+  const requestSchools = () => {
+    getFiltersInfo()
+      .then((res) => {
+        setSchoolsList(res.data.schools);
+        setProfessorsList(res.data.professors);
+        setDisciplinesList(res.data.disciplines);
+        setTypesList(res.data.types);
+        setSemestersList(res.data.semesters);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(schoolsList);
+      });
+  };
+
+  useEffect(() => {
+    requestSchools();
+  }, []);
+
   return (
     <StyledPageContainer>
       <StyledMainContent>
-        sdfjnsodafjnsod
+        <StyledFormContainer>
+          <StyledForm>
+            <label htmlFor="filters" className="inputs-container">
+              Choose your filters:
+              <input list="schools" name="schools" id="filters" />
+              <input list="professors" name="professors" id="filters" />
+              <input list="types" name="types" id="filters" />
+              <input list="disciplines" name="disciplines" id="filters" />
+              <input list="semesters" name="semesters" id="filters" />
+            </label>
+            <datalist id="schools">
+              {schoolsList.map((school) => (
+                <option value={school} />
+              ))}
+            </datalist>
+            <datalist id="professors">
+              {professorsList.map((school) => (
+                <option value={school} />
+              ))}
+            </datalist>
+            <datalist id="disciplines">
+              {disciplinesList.map((school) => (
+                <option value={school} />
+              ))}
+            </datalist>
+            <datalist id="types">
+              {typesList.map((school) => (
+                <option value={school} />
+              ))}
+            </datalist>
+            <datalist id="semesters">
+              {semestersList.map((school) => (
+                <option value={school} />
+              ))}
+            </datalist>
+            <StyledSubmit type="submit" className="submit" />
+          </StyledForm>
+        </StyledFormContainer>
       </StyledMainContent>
     </StyledPageContainer>
   );
@@ -25,4 +91,36 @@ const StyledMainContent = styled.div`
   align-items: center;
   justify-content: center;
   gap: 5vh;
+`;
+const StyledFormContainer = styled.div`
+  width: 100vw;
+`;
+const StyledForm = styled.form`
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  .inputs-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+  }
+`;
+const StyledSubmit = styled.input`
+  width: 200px;
+  height:70px;
+  border-radius: 60px;
+  background-color: #bfbafc;
+  font-size: 20px;
+  font-weight: 700;
+  color: #ffffff;
+  cursor: pointer;
+  border: none;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API_URL = '';
+const API_URL = 'http://localhost:4000';
 
-const createHeaders = (token) => ({
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
-});
+// const createHeaders = (token) => ({
+//   headers: {
+//     Authorization: `Bearer ${token}`,
+//   },
+// });
 
-const example = (body, token) => axios.post(`${API_URL}/example`, body, createHeaders(token));
+const getFiltersInfo = () => axios.get(`${API_URL}/schools`);
 
 export {
-  example,
+  getFiltersInfo,
 };
