818a37361d526e50ce4f4dce0d5febc6e8dd6000
PMafra <pmafrarm@gmail.com>
2021-12-18 19:23:21 -0300

    Remove comment from api file

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:4000';
 
-// const createHeaders = (token) => ({
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//   },
-// });
-
 const getSchools = () => axios.get(`${API_URL}/filters/schools`);
 const getCategories = () => axios.get(`${API_URL}/filters/categories`);
 const getProfessorsByFilter = (body) => axios.post(`${API_URL}/filters/professors`, body);
